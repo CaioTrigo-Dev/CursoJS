b37329567b5feae70973e824134a9b333f1b405c
CaioTrigo-Dev <caiorj95@gmail.com>
2024-09-23 22:20:04 +0100

    Delete aula21 directory

--- aula21/js/script.js
+++
@@ -1,7 +0,0 @@
-let num1 = prompt("Digite um numero: ");
-let num2 = prompt("Digite outro Numero: ");
-let num3 = parseInt(num1);
-let num4 = parseInt(num2);
-const resultado = num3 + num4;
-
-alert('O resultado é ' + resultado)
